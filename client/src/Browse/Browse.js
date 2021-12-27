d660bbdab990e09c87df751675105c1b1335349c
Ethan Horowitz <ethan.horowitz@duke.edu>
2021-12-26 21:00:03 -0700

    not sure but I'll go with it

--- client/src/Browse/Browse.js
+++ client/src/Browse/Browse.js
@@ -22,9 +22,17 @@ const Browse = () => {
         }
     };
 
+    const detectEnter = (e) => {
+        if (e.key === 'Enter') history.push('/search/' + search);
+    }
+
     useEffect(() => {
         let isMounted = true;
-        fetch('/testsession').then(res => res.json()).then(json => {
+        fetch('/api').then(res => res.text()).then(text => { 
+            console.log('TESTING');
+            console.log(text);
+        })
+        fetch('/testsession').then(res => res.text()).then(json => {
             console.log(json);
             if (isMounted && !('error' in json)){
                 console.log('logged in');
@@ -47,7 +55,7 @@ const Browse = () => {
         <div onClick={bodyClick}>
             <Header showModal={modal} setModal={showModal} logged={loggedIn} name={name} uid={uid} menu={menu} showMenu={showMenu} />
             <main className='text-center transition duration-300' style={{filter: (modal ? "blur(10px)" : 'blur(0)')}}>
-                <input onChange={(e) => editSearch(e.target.value)} type="text" value={search} placeholder="Search" className="bg-gray-200 p-2 text-xl rounded-md mb-4 outline-none w-96 border-2 border-gray-400 " />
+                <input onChange={(e) => editSearch(e.target.value)} onKeyPress={detectEnter} type="text" value={search} placeholder="Search" className="bg-gray-200 p-2 text-xl rounded-md mb-4 outline-none w-96 border-2 border-gray-400 " />
                 <h1 className="text-xl font-thin">Search results for &quot;{query}&quot;</h1>
             </main>
         </div>
